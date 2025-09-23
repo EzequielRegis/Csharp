e319b5a2422ad7ab2b15f5555ef4172d9b7c44dd
EzequielRegis <regisezequiel@yahoo.com>
2025-09-23 12:14:46 -0300

    Agregadas cositas

--- AdivinarNumero/AdivinarNumero/Form1.Designer.cs
+++ AdivinarNumero/AdivinarNumero/Form1.Designer.cs
@@ -47,7 +47,8 @@
             // 
             // lblResultado
             // 
-            this.lblResultado.BackColor = System.Drawing.Color.Firebrick;
+            this.lblResultado.BackColor = System.Drawing.Color.Brown;
+            this.lblResultado.ForeColor = System.Drawing.Color.Black;
             this.lblResultado.Location = new System.Drawing.Point(146, 180);
             this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblResultado.Name = "lblResultado";
@@ -68,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Firebrick;
+            this.BackColor = System.Drawing.Color.Brown;
             this.ClientSize = new System.Drawing.Size(517, 263);
             this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.lblResultado);
